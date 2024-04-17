b35640c4b187955e43c3f814d2ef439a3a32ad02
sunnatbek07 <sunnatbekqadamov9@gmail.com>
2024-04-17 13:09:53 +0500

    removed comments in code

--- src/components/Ui/Carousel/index.jsx
+++ src/components/Ui/Carousel/index.jsx
@@ -15,10 +15,6 @@ const Carousel = () => {
       <Swiper
         spaceBetween={30}
         centeredSlides={true}
-        // autoplay={{
-        //   delay: 2500,
-        //   disableOnInteraction: false,
-        // }}
         pagination={{
           clickable: true,
         }}
